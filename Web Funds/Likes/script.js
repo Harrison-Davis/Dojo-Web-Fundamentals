a15e237b574185a4e1b8c780730f862df3efe528
Harrison-Davis <99163784+Harrison-Davis@users.noreply.github.com>
2022-03-01 23:56:59 -0800

    w part 2

--- Web Funds/Likes/script.js
+++ Web Funds/Likes/script.js
@@ -1,13 +1,42 @@
 
-var count = 3
-var countElem = document.querySelector("#count");
 
-console.log(countElem);
+// var likeNum = 3
+// var countElem = document.querySelector("#likeNum");
 
-function addLikes() {
-    count++;
-    countElem.innerText = count + " like(s)"
-    console.log(count);
+// console.log(countElem);
+
+// function addLikes() {
+//     likeNum++;
+//     countElem.innerText = likeNum + " like(s)"
+//     console.log(likeNum);
+// }
+
+
+
+// var likeNum = 3
+
+// console.log(countElem);
+
+// function addLikes(elemId) {
+//     var countElem = document.querySelector(elemId);
+//     likeNum++;
+//     countElem.innerText = likeNum 
+//     console.log(likeNum);
+// }
+
+
+
+
+// function addLikes(elemId) {
+//     var countElem = document.querySelector(elemId);
+//     var likeNum = 3
+//     likeNum++
+//     countElem.innerText = likeNum++
+//     console.log(likeNum);
+// }
+
+
+function addLikes(elemId) {
+    var h2 = elemId.parentNode.querySelector('h2')
+    h2.innerText = parseInt(span.innerText) + 1
 }
-
-
